09cce1e2a60a49f32337704354e20839733eae3b
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-02-16 19:00:16 +0530

    Update recentlyviewed.js with adding timestamp

--- backend/models/recentlyviewed.js
+++ backend/models/recentlyviewed.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const recentlyviewedSchema = new Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, default: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, default: true },
-  viewTime: {type: Date, default: Date.now }
-});
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'courses'  },
+}, { timestamps: true });
 
 module.exports = mongoose.model('recentlyviewed', recentlyviewedSchema);
