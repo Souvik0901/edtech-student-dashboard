72044b7056c199084eca6e41e5dec342e40423ef
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-02-16 12:29:50 +0530

    add viewTime

--- backend/models/recentlyviewed.js
+++ backend/models/recentlyviewed.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const recentlyviewedSchema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, default: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, default:true },
+  courseId: { type: mongoose.Schema.Types.ObjectId, default: true },
+  viewTime: {type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('recentlyviewed', recentlyviewedSchema);
