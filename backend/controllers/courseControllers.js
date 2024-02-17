8c3f270efdde74c2c2c033f21fe4a3cab99ca228
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-02-17 13:28:43 +0530

    update getSingleCourse, getRecentlyView

--- backend/controllers/courseControllers.js
+++ backend/controllers/courseControllers.js
@@ -5,7 +5,6 @@ const RecentlyViewed = require('../models/recentlyviewed');
 const ResponseObjectClass = require('../helpers/ResponseObject');
 const newResponseObject = new ResponseObjectClass();
 
-
 // create a  single course
 const createCourseWithImage = async (req, res) => {
   try {
@@ -158,15 +157,18 @@ const getSingleCourse = async (req, res) => {
       );
     }
 
-    // Store the recently viewed course 
-    let recentlyViewedCourse = await RecentlyViewed.findOne({ userId, courseId: id });
+ // Store the recently viewed course 
+    let recentlyViewedCourse = await RecentlyViewed.findOneAndUpdate(
+      { userId, courseId: id },
+      { 
+        $set: { 
+          updatedAt: new Date(),
+          createdAt: { $cond: [{ $eq: ["$createdAt", null] }, new Date(), "$createdAt"] }
+        } 
+      },
+      { upsert: true, new: true }
+    );
 
-    if (!recentlyViewedCourse) {
-      recentlyViewedCourse = await RecentlyViewed.create({ userId, courseId: id });
-    } else {
-      recentlyViewedCourse.updatedAt = new Date();
-      await recentlyViewedCourse.save();
-    }
  
     return res.send(
       newResponseObject.create({
@@ -192,7 +194,11 @@ const getRecentlyViewedCourses = async (req, res) => {
   const { userId } = req.user;
 
   try {
-    const recentlyViewed = await RecentlyViewed.find({ userId }).sort({ updatedAt: -1 }).populate('courseId').exec();
+    const recentlyViewed = await RecentlyViewed.find({ userId })
+    .sort({ updatedAt: -1 })
+    .limit(5)
+    .populate('courseId')
+    .exec();
 
     return res.send({
       code: 200,
@@ -209,31 +215,6 @@ const getRecentlyViewedCourses = async (req, res) => {
     });
   }
 };
-
-// clear all recently-view courses
-const clearAllViewedCourses = async (req, res) => {
-  const { userId } = req.user;
-
-  try {
-    // Delete all recently viewed courses for the user
-    await RecentlyViewed.deleteMany({ userId });
-
-    return res.send({
-      code: 200,
-      success: true,
-      message: 'All viewed courses cleared successfully',
-      data: {},
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      success: false,
-      message: 'Internal server error',
-    });
-  }
-};
-
 
 // get all paginated courses
 const paginatedCourses = async (req, res) => {
@@ -421,6 +402,31 @@ const updateCourse = async (req, res) => {
   }
 };
 
+// clear all recently-view courses
+const clearAllViewedCourses = async (req, res) => {
+  const { userId } = req.user;
+
+  try {
+    // Delete all recently viewed courses for the user
+    await RecentlyViewed.deleteMany({ userId });
+
+    return res.send({
+      code: 200,
+      success: true,
+      message: 'All viewed courses cleared successfully',
+      data: {},
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      success: false,
+      message: 'Internal server error',
+    });
+  }
+};
+
+
 module.exports = {
   createCourseWithImage,
   paginatedCourses,
@@ -431,4 +437,5 @@ module.exports = {
   getRecentlyViewedCourses,
   clearAllViewedCourses
 };
+
 
