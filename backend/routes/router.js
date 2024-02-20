e6beef19c6adc8bb0effd8fea58dd7616fdb3249
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-02-20 12:30:02 +0530

    cart api added

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -10,6 +10,8 @@ import getUserControllers from '../controllers/getUserControllers';
 import editProfile from '../controllers/editProfile';
 import deleteAccount from '../controllers/deleteAccount';
 import enrollmentList from '../controllers/enrollmentList';
+import cartControllers from '../controllers/cartControllers';
+import orderControllers from '../controllers/orderControllers';
 
 /* Importing the functions from middleware files. */
 import authenticateUser from '../middleware/authenticateUser';
@@ -28,41 +30,35 @@ const upload = multer({ storage: fileStorageEngine });
 
 /* Creating a new router object. */
 const router = express.Router();
-
 router.use('/images', express.static('server/uploads'));
 
+
 router.get('/health-check', checkConnection);
+
 router.post('/login', userController.login);
 router.post('/signup', userController.signup);
 router.post('/logout', userController.logout);
+
+router.post('/addtocart', authenticateUser.verifytoken, cartControllers.addCartitems);
+router.get('/getcartitems', authenticateUser.verifytoken, cartControllers.getCartitems);
+router.post('/removecartitems', authenticateUser.verifytoken, cartControllers.removeCartitems);
+router.post('/emptycart', authenticateUser.verifytoken, cartControllers.emptyCart);
+
+router.post('/addorders', authenticateUser.verifytoken, orderControllers.addOrderitems);
+router.get('/getorderitems', authenticateUser.verifytoken, orderControllers.getOrderitems);
+
 router.get('/getuserdata', authenticateUser.verifytoken, getUserControllers.getUser);
 router.get('/removeuser', authenticateUser.verifytoken, deleteAccount.removeUser);
 router.get('/getenrollmentlist', authenticateUser.verifytoken, enrollmentList.getStudentDetails);
-router.post(
-  '/updateprofileinfo',
-  authenticateUser.verifytoken,
-  upload.single('profileImg'),
-  editProfile.changeUserdata,
-);
-router.get('/getsingleuser/:id', authenticateUser.verifytoken, getUserControllers.getSingleUser);
+router.post('/updateprofileinfo',authenticateUser.verifytoken,upload.single('profileImg'),editProfile.changeUserdata,);
 
-router.post(
-  '/createcoursewithimage',
-  authenticateUser.verifytoken,
-  upload.single('courseImage'),
-  courseControllers.createCourseWithImage,
-);
+
+router.post('/createcoursewithimage',authenticateUser.verifytoken,upload.single('courseImage'),courseControllers.createCourseWithImage,);
 router.get('/getcourses', authenticateUser.verifytoken, courseControllers.getCourses);
 router.get('/getsinglecourse/:id', authenticateUser.verifytoken, courseControllers.getSingleCourse);
 router.get('/paginatedcourses', authenticateUser.verifytoken, courseControllers.paginatedCourses);
 router.delete('/deletecourse/:id', authenticateUser.verifytoken, courseControllers.deleteCourse);
-router.patch(
-  '/updatecourse/:id',
-  authenticateUser.verifytoken,
-  upload.single('courseImage'),
-  courseControllers.updateCourse,
-);
-
+router.patch( '/updatecourse/:id',authenticateUser.verifytoken,upload.single('courseImage'),courseControllers.updateCourse,);
 router.get('/recentlyview',authenticateUser.verifytoken,courseControllers.getRecentlyViewedCourses,);
 router.delete('/clearrecentlyview',authenticateUser.verifytoken, courseControllers.clearAllViewedCourses,);
 
